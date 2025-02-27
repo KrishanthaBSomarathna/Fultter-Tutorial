83d08f00365f894a7b8b34b6af8773c9cebb8c48
Krishantha Bandara <info.krishantha.b@gmail.com>
2025-02-27 19:58:47 +0530

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(
+      enabled: true, // Set to false to disable preview
+      builder: (context) => MyApp(), // Replace MyApp with your main app widget
+    ),);
 }
 
-stl
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      useInheritedMediaQuery: true, // Required for DevicePreview to work
+      locale: DevicePreview.locale(context), // Adjust locale dynamically
+      builder: DevicePreview.appBuilder, // Wrap your app with DevicePreview
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+    );
+  }
+}
