c742202145c7a25b807e359cca19e1ce59f2c9e6
Krishantha Bandara <info.krishantha.b@gmail.com>
2025-02-27 20:18:24 +0530

    layoutthree

--- lib/main.dart
+++ lib/main.dart
@@ -13,18 +13,105 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+     return MaterialApp(
       useInheritedMediaQuery: true, // Required for DevicePreview to work
       locale: DevicePreview.locale(context), // Adjust locale dynamically
       builder: DevicePreview.appBuilder, // Wrap your app with DevicePreview
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        
+        appBar: AppBar(backgroundColor: Color(0xff9E00FF)),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 175,
+                    height: 175,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xff06FFA5),
+                          spreadRadius: 5.0,
+                          offset: Offset(2.0, 2.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 175,
+                    height: 175,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xff06FFA5),
+                          spreadRadius: 5.0,
+                          offset: Offset(2.0, 2.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                width: double.infinity,
+                height: 175,
+                color: Color(0xff06FFA5),
+              ),
+
+              SizedBox(
+                height: 350,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: 175,
+                          height: 250,
+                          color: Color(0xffFFE500),
+                        ),
+                        Container(
+                          width: 175,
+                          height: 100,
+                          color: Color(0xffFFE500),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: 175,
+                          height: 100,
+                          color: Color(0xff7752FE),
+                        ),
+                        Container(
+                          width: 175,
+                          height: 100,
+                          color: Color(0xff0E00AC),
+                        ),
+                        Container(
+                          width: 175,
+                          height: 100,
+                          color: Color(0xff7752FE),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      
-
-
     );
   }
 }
